41b193a4e6c938f58b01fde94429e596bcf9bc4b
andybeet <22455149+andybeet@users.noreply.github.com>
2019-05-22 12:43:18 -0400

    additional output variables - - recSigma. associated error value to simulated SR form - recType. assigned recruitment Type as required by Hydra

--- R/simulate_parameters.R
+++ R/simulate_parameters.R
@@ -2,30 +2,42 @@
 #'
 #' Currently only simulate stock recruitment parameters and other food. See Method section below for more details
 #'
-#' @param stockRecruitParams
-#' @param simulationRules These have been lazily loaded as darwinRules
+#' @param stockRecruitParams Data set similar to lazily loaded darwinData dataset
+#' @param simulationRules Set of rules set up similar to lazily loaded darwinRules dataset
+#' @param SRFunctionChoice Numeric Vector. Whether the Stock recruitment function for each species should be piecewise model (hockey, generalized,=  0) or shepherd (1) or a random choice of either (Default = NULL)
 #'
 #' @return List
 #' \item{otherFood}{simulated value of otherFood variable}
 #' \item{alphaShepherd}{simulated value of alpha for the shepherd model  }
 #' \item{betaShepherd}{simulated value of beta for the shepherd model}
 #' \item{shapeShepherd}{value of the shape parameter. Predefined by user in darwinRules$SRType}
-#' \item{alphaHockey}{}
-#' \item{betaHockey}{}
-#' \item{shapeHockey}{}
-#' \item{alphaSegmented}{}
-#' \item{alphaSegmented}{}
-#' \item{alphaSegmented}{}
+#' \item{alphaHockey}{simulated value of alpha (slope) for the hockey stick model}
+#' \item{betaHockey}{simulated value of beta (-alpha) for the hockey stick model}
+#' \item{shapeHockey}{simulated value of the breakpoint for the hockey stick model}
+#' \item{alphaSegmented}{simulated value of alpha (slope) for the generalized segmented model}
+#' \item{betaSegmented}{simulated value of beta (change in the slope) for the generalized segmented model}
+#' \item{shapeSegmented}{simulated value of breakpoint for the generalized segmented model}
+#' \item{recSigma}{Values of sigma to be used in Hydra for each species/stock recruitment function chosen}
+#' \item{recType}{Denotes the form of the SR model used in Hydra. see field \code{recType} in \code{hydradata::hydraDataList} }
 #'
 #' @section Method:
-#' Methods are described in the vignette \code{simulation_documentation}
+#' Methods and \code{darwinRules} dataset are described in the vignette \code{simulation_documentation}
+#' The data set \code{darwinData} is defined in \code{?darwinData} and explained futher in the vignette \code{darwinData_explained}
 #'
 #' @export
 
-simulate_parameters <- function(stockRecruitParams,simulationRules){
+simulate_parameters <- function(stockRecruitParams=darwinData,simulationRules=darwinRules,SRFunctionChoice=NULL){
   nSpecies <- length(simulationRules$SRType)
   simulatedValues <- list()
   simValues <- list()
+
+  if (is.null(SRFunctionChoice)){ # ramdon sample
+    SRFunctionChoice <- sample(c(0,1),nSpecies,replace=TRUE)
+  } else {
+    if ((length(SRFunctionChoice) != nSpecies) | (!all(SRFunctionChoice %in% c(0,1)))){
+      stop(paste0("SRFunctionChoice must be a vector (of 0's and 1's) of length = ",nSpecies))
+    }
+  }
   # otherfood
   otherFoodChoices <- simulationRules$otherFood
   simulatedValues$otherFood <-  sample(otherFoodChoices,1)
@@ -33,24 +45,43 @@ simulate_parameters <- function(stockRecruitParams,simulationRules){
   shepherd <- vector(mode="numeric",length=nSpecies) # allocate for shepherd SR function
 
   # simulate from a shepherd SR function based on ricker shape and beverton holt shape
-  simValues$Shepherd <- simulate_shepherd_SR(stockRecruitParams$Ricker,"ricker",simulationRules$RickerRangeOfAlphas)
-  bh <- simulate_shepherd_SR(stockRecruitParams$BH,"bh",simulationRules$BHRangeOfAlphas)
+  simValues$Shepherd <- simulate_shepherd_SR(stockRecruitParams$Ricker,"ricker",simulationRules$RickerRangeOfAlphas,nSpecies)
+  bh <- simulate_shepherd_SR(stockRecruitParams$BH,"bh",simulationRules$BHRangeOfAlphas,nSpecies)
   # assign values based on whether a species is defined as overcompensatory or not. If over compensatory do nothing otherwise use BH fit
   indBH <- simulationRules$SRType == 1 # not overcompensatory
   simValues$Shepherd$alpha[indBH] <- bh$alpha[indBH]
   simValues$Shepherd$beta[indBH] <- bh$beta[indBH]
   simValues$Shepherd$shape[indBH] <- bh$shape[indBH]
 
-  simValues$Hockey <- simulate_hockey_SR(stockRecruitParams$hockey,stockRecruitParams$SSB,simulationRules$rangeOfBreakpoint)
-  simValues$Segmented <- simulate_segmented_SR(stockRecruitParams$segmented,stockRecruitParams$SSB,simulationRules$rangeOfBreakpoint)
+  simValues$Hockey <- simulate_hockey_SR(stockRecruitParams$hockey,stockRecruitParams$SSB,simulationRules$rangeOfBreakpoint,nSpecies)
+  simValues$Segmented <- simulate_segmented_SR(stockRecruitParams$segmented,stockRecruitParams$SSB,simulationRules$rangeOfBreakpoint,nSpecies)
 
   # rename list fields to be consistent with HydraData
   shepherdList <- rename_variables(simValues,"Shepherd")
   segmentedList <- rename_variables(simValues,"Segmented")
   hockeyList <- rename_variables(simValues,"Hockey")
+
+  # use the values of sigma for respective fits. If choose BH then use sigma associated with beverton holt fit
+  recSigma <- vector(mode="numeric",length=nSpecies)
+  indS <- (simulationRules$SRType == 2) & (SRFunctionChoice == 0)
+  recSigma[indS] <- stockRecruitParams$segmented$sigma[indS]
+  indR <- (simulationRules$SRType == 2) & (SRFunctionChoice == 1)
+  recSigma[indR] <- stockRecruitParams$Ricker$sigma[indR]
+  indH <- (simulationRules$SRType == 1) & (SRFunctionChoice == 0)
+  recSigma[indH] <- stockRecruitParams$hockey$sigma[indH]
+  indB <- (simulationRules$SRType == 1) & (SRFunctionChoice == 1)
+  recSigma[indB] <- stockRecruitParams$BH$sigma[indB]
+
+  # assign the approriate flag to recType. Hydra uses this to simulate from a defined SR function
+  # 6 = shepher, 7 = hockey, 8 = segmented
+  recType <- SRFunctionChoice * 6 # set all 1's to shepherd
+  recType[(SRFunctionChoice==0) & (simulationRules$SRType == 2)] <- 8 # set some 0's to 8
+  recType[(SRFunctionChoice==0) & (simulationRules$SRType == 1)] <- 7 # set some 0's to 7
+
   # combine fields
   simulatedValues <- c(simulatedValues,shepherdList,segmentedList,hockeyList)
-
+  simulatedValues$recSigma <- recSigma
+  simulatedValues$recType <- recType
 
   return(simulatedValues)
 }
@@ -72,7 +103,7 @@ rename_variables <- function(variableNm,fieldName){
 
 
 #
-simulate_shepherd_SR <- function(estimatedParams,form,rangeOfAlphas){
+simulate_shepherd_SR <- function(estimatedParams,form,rangeOfAlphas,nSpecies){
   params <- list()
   alpha_l <- rangeOfAlphas[1]
   alpha_u <- rangeOfAlphas[2]
@@ -93,9 +124,8 @@ simulate_shepherd_SR <- function(estimatedParams,form,rangeOfAlphas){
   return(params)
 }
 
-simulate_hockey_SR <- function(estimatedParams,SSB,breakpointRule){
+simulate_hockey_SR <- function(estimatedParams,SSB,breakpointRule,nSpecies){
   params <- list()
-  nSpecies <- dim(estimatedParams)[1]
 
   params$shape <- runif(nSpecies,breakpointRule[1]*SSB$min_obs_SS,breakpointRule[2]*estimatedParams$shape)
   params$alpha <- estimatedParams$alpha*estimatedParams$shape/params$shape
@@ -103,9 +133,8 @@ simulate_hockey_SR <- function(estimatedParams,SSB,breakpointRule){
   return(params)
 
 }
-simulate_segmented_SR <- function(estimatedParams,SSB,breakpointRule){
+simulate_segmented_SR <- function(estimatedParams,SSB,breakpointRule,nSpecies){
   params <- list()
-  nSpecies <- dim(estimatedParams)[1]
   betaSim <- vector(mode="numeric",length=nSpecies)
 
   # new breakpoint is in interval [% (50)minObsSSB , % (200)fittedShape]
